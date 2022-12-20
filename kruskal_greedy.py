e8c2ec34ffb1d9578e0447c936aea8b3f054d4be
Chris <70657828+BabalauCristian10C@users.noreply.github.com>
2022-12-20 23:28:04 +0500

    Add files via upload

---
+++ kruskal_greedy.py
@@ -0,0 +1,58 @@
+﻿"""
+Considerăm un graf neorientat ponderat (cu costuri) conex G.
+Se numește arbore parțial un graf parțial al lui G care este arbore. Se numește arbore parțial de cost minim un arbore parțial pentru care suma costurilor muchiilor este minimă.
+
+Dacă graful nu este conex, vorbim despre o pădure parțială de cost minim.
+
+Să se determine unu arbore parțial de cost minim (APM) într-un graf ponderat cu N noduri.
+"""
+
+class graf:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graful = []
+    def adauga_margine(self, u, v, w):
+        self.graful.append([u, v, w])
+    def gaseste(self, parental, i):
+        if parental[i] != i:
+            parental[i] = self.gaseste(parental, parental[i])
+        return parental[i]
+    def union(self, parental, grad, x, y):
+        if grad[x] < grad[y]:
+            parental[x] = y
+        elif grad[x] > grad[y]:
+            parental[y] = x
+        else:
+            parental[y] = x
+            grad[x] += 1
+    def GreedyAPM(self):
+        rezultat,i,e = [],0,0
+        self.graful = sorted(self.graful,key=lambda item: item[2])
+        parental = []
+        grad = []
+        for node in range(self.V):
+            parental.append(node)
+            grad.append(0)
+        while e < self.V - 1:
+            u, v, w = self.graful[i]
+            i = i + 1
+            x = self.gaseste(parental, u)
+            y = self.gaseste(parental, v)
+            if x != y:
+                e = e + 1
+                rezultat.append([u, v, w])
+                self.union(parental, grad, x, y)
+        minim = 0
+        print("Nodurile din APM-ul construit:")
+        for u, v, pret in rezultat:
+            minim += pret
+            print("%d -- %d == %d" % (u, v, pret))
+        print("Arborele parțial de cost minim (APM):", minim)
+
+tabel = graf(4)
+tabel.adauga_margine(0, 1, 10)
+tabel.adauga_margine(0, 2, 6)
+tabel.adauga_margine(0, 3, 5)
+tabel.adauga_margine(1, 3, 15)
+tabel.adauga_margine(2, 3, 4)
+tabel.GreedyAPM()
---
+++ triere.py
@@ -0,0 +1,16 @@
+﻿"""
+Să se scrie un program care determină toate secventele binare de lungime n,
+fiecare din ele conținînd nu mai puțin de k cifre de 1.
+"""
+
+n=int(input("Introduceti lungimea secventei binare: "))
+k=int(input("Introduceti numarul cifrelor de 1: "))
+
+def sol_pos(l):
+    if list('{0:b}'.format(l)).count("1")>=k:return True
+    else:return False
+def prel_sol(x):print('{0:b}'.format(x))
+
+for i in range(2**n):
+    if sol_pos(i):
+        prel_sol(i)
